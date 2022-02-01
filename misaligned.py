e44c94101733b6ceeba642760546eed90edf3ffc
Patiljyoti12 <59950788+Patiljyoti12@users.noreply.github.com>
2022-02-01 17:05:00 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -1,24 +1,21 @@
-
 major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
 minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
- 
- 
 color_code_manual=[]
-def print_color_map(major_colors,minor_colors):   
-    for i, major in enumerate(major_colors):
-        for j, minor in enumerate(minor_colors):
-            print(f'{i * 5 + j} | {major} | {minor}')
-            color_code_manual.append(f'{i * 5 + j}| {major} | {minor}')
-            length_major_minor_colors(major_colors,minor_colors)
+
+def get_pair_number_from_colors(major_colors,minor_colors):   
+    for i, major_col in enumerate(major_colors):
+        for j, minor_col in enumerate(minor_colors):
+         color_code_manual.append([i * 5 + j + 1 ,major_col,minor_col])   
     return color_code_manual
             
     
     
-def length_major_minor_colors(major_colors,minor_colors):  
-        return len(major_colors) * len(minor_colors)
+
         
-result=print_color_map(major_colors,minor_colors)
-assert (result[0]=='1| White | Blue')
-assert (result[24]=='25| Violet | Slate')
+result=get_pair_number_from_colors(major_colors,minor_colors)
+assert (result[0]==[1,'White' ,'Blue'])
+assert ('White' in color_code_manual[0])
+assert ('Blue' in color_code_manual[0])
+assert (result[24]==[25, 'Violet', 'Slate'])
 print("All is well (maybe!)\n")
 
